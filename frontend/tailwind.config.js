8dfc7c06c9ccd07fdfebf2645aae826431236681
John Doe <johndoe@email.com>
2024-12-23 12:34:09 +0700

    install tailwind, update profile UI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Bao gồm tất cả file trong thư mục src
+    "./public/index.html",             // Đường dẫn file HTML chính
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
